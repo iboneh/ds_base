cbd3ce7d40f8f9e749cea630b46e0d7646dc25b9
Stanislav Zelentsov <iboneh@users.noreply.github.com>
2020-03-14 07:47:48 +0300

    Create my_app.py

---
+++ sf_module_0/my_app.py
@@ -0,0 +1,20 @@
+"""
+## APP NAME
+ 
+DESCRIPTION
+ 
+Author: [YOUR NAME](https://URL_TO_YOU))\n
+Source: [Github](https://github.com/URL_TO_CODE)
+"""
+import streamlit as st
+ 
+# Your imports goes below
+ 
+def main():
+    st.title("APP NAME")
+    st.markdown("DESCRIPTION")
+ 
+    # Your code goes below
+ 
+if __name__ == "__main__":
+    main()
